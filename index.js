d06dc64aaf67e9b2b28f85decc1b351b7810ea3e
Adrian Kothman <adrian@veerwest.com>
2015-07-25 16:28:09 -0400

    add logit for icon onclick

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var self = require('sdk/self');
+var buttons = require('sdk/ui/button/action');
+var tabs = require('sdk/tabs')
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var button = buttons.ActionButton({
+    id: "Octo-Image",
+    label: "Replace!",
+    icon: {
+        "16": "./icon-16.png",
+        "32": "./icon-32.png",
+        "64": "./icon-64.png",
+    },
+    onClick: handleClick
+});
+
+function handleClick(state) {
+    tabs.activeTab.attach({
+        contentScript: self.url("replace.js")
+    });
 }
-
-exports.dummy = dummy;
